3ab09215ea73a681e91266641bd5c10a357db9b0
Schemb <joseph.schembri@student.adelaide.edu.au>
2025-06-20 12:22:52 +0000

    [automatic commit] 2025-06-20 12:22:52 UTC 115.131.34.193

--- part1/routes/api.js
+++ part1/routes/api.js
@@ -91,7 +91,7 @@ router.get('/dogs', async (req, res) => {
   }
 });
 
-router.get('/dogs', async (req, res) => {
+router.get('/walkrequests/open', async (req, res) => {
   try {
     const [dogs] = await db.execute('SELECT * FROM Dogs');
     res.json(dogs);
