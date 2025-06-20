d981ef0bbaedc6228e6323b42aec3aa82ae14354
Schemb <joseph.schembri@student.adelaide.edu.au>
2025-06-20 13:00:35 +0000

    [automatic commit] 2025-06-20 13:00:35 UTC 115.131.34.193

--- part1/routes/api.js
+++ part1/routes/api.js
@@ -81,7 +81,7 @@ let db;
     if (rows[0].count === 0) {
       await db.execute(`
         INSERT INTO WalkRatings (request_id, walker_id, owner_id, rating, comments)
-        SELECT Dogs.name AS dog_name, Dogs.size, Users.username AS owner_username FROM Dogs INNER JOIN Users ON Dogs.owner_id = Users.user_id LIMIT 1
+        SELECT WalkRequests.request_id, Dogs.size, Users.username AS owner_username FROM Dogs INNER JOIN Users ON Dogs.owner_id = Users.user_id LIMIT 1
       `);
       await db.execute(`
         INSERT INTO WalkRequests (dog_id, requested_time, duration_minutes, location, status)
