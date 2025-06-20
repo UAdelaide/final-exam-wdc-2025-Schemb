e8aa6434df24eaac242a8b9fe08a51072af6e474
Schemb <joseph.schembri@student.adelaide.edu.au>
2025-06-20 11:57:44 +0000

    [automatic commit] 2025-06-20 11:57:44 UTC 115.131.34.193

--- part1/app.js
+++ part1/app.js
@@ -86,7 +86,7 @@ let db;
       `);
       await db.execute(`
         INSERT INTO WalkRequests (dog_id, requested_time, duration_minutes, location, status)
-        SELECT dog_id, NOW(), 1, 'Tinyville', 'cancelled' FROM `Dogs` WHERE `name` = 'Pipsqueak' LIMIT 1
+        SELECT dog_id, NOW(), 1, 'Tinyville', 'cancelled' FROM Dogs WHERE name = 'Pipsqueak' LIMIT 1
       `);
     }
   } catch (err) {
