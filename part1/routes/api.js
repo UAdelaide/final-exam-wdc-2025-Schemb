6c0337fd2e89bfba7bddfc4431af80545e3b51a7
Schemb <joseph.schembri@student.adelaide.edu.au>
2025-06-20 12:01:25 +0000

    [automatic commit] 2025-06-20 12:01:25 UTC 115.131.34.193

--- part1/routes/api.js
+++ part1/routes/api.js
@@ -2,8 +2,13 @@ var express = require('express');
 var router = express.Router();
 
 /* GET dogs page. */
-router.get('/dogs', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', async (req, res) => {
+  try {
+    const [books] = await db.execute('SELECT * FROM books');
+    res.json(books);
+  } catch (err) {
+    res.status(500).json({ error: 'Failed to fetch books' });
+  }
 });
 
 module.exports = router;
