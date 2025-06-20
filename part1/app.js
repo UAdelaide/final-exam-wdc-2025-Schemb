749f97ce362a4d7fca7a8a74aff1d6c5eb374b1c
Schemb <joseph.schembri@student.adelaide.edu.au>
2025-06-20 11:34:45 +0000

    [automatic commit] 2025-06-20 11:34:45 UTC 115.131.34.193

--- part1/app.js
+++ part1/app.js
@@ -38,7 +38,7 @@ let db;
       `);
     }
   } catch (err) {
-    console.error('Error setting up database. Ensure 'Mysql is running: service mysql start'', err);
+    console.error('Error setting up database. Ensure dogwalks.sql is loaded into mysql', err);
   }
 })();
 
