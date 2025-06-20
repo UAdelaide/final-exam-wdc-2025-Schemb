7eed8d1a5529c7d909d4d261d8567b65aaab55c7
Schemb <joseph.schembri@student.adelaide.edu.au>
2025-06-20 23:35:08 +0000

    [automatic commit] 2025-06-20 23:35:08 UTC ;; communications error to 208.67.222.222#53: timed out ;; communications error to 208.67.222.222#53: timed out 115.131.34.193

--- part1/routes/api.js
+++ part1/routes/api.js
@@ -113,7 +113,7 @@ router.get('/walkrequests/open', async (req, res) => {
 /* GET open walkers summary page. */
 router.get('/walkers/summary', async (req, res) => {
   try {
-    const [openWalkRequests] = await db.execute(`SELECT Users.username AS walker_username, total_ratings, average_rating, completed_walks FROM Users INNER JOIN WalkRatings ON Users.user_id = WalkRatings.walker_id WHERE total_ratings = (SELECT COUNT(*) FROM WalkRatings WHERE )`);
+    const [openWalkRequests] = await db.execute(`SELECT Users.username AS walker_username, total_ratings, average_rating, completed_walks FROM Users INNER JOIN WalkRatings ON Users.user_id = WalkRatings.walker_id WHERE total_ratings = (SELECT COUNT(*) FROM WalkRatings WHERE walker)`);
     res.json(openWalkRequests);
   } catch (err) {
     res.status(500).json({ error: 'Failed to fetch walkers summary' });
