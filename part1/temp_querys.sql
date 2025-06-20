e81c5ee4e6f733861db4b55a1420e481bf0dbcde
Schemb <joseph.schembri@student.adelaide.edu.au>
2025-06-20 07:28:04 +0000

    [automatic commit] 2025-06-20 07:28:04 UTC 115.131.34.193

--- part1/temp_querys.sql
+++ part1/temp_querys.sql
@@ -27,3 +27,9 @@ VALUES ('bobwalker', 'bob@example.com', 'hashed456', walker);
 
 INSERT INTO `Users` (`username`, `email`, `password_hash`, `role`)
 VALUES ('carol123', 'carol@example.com', 'hashed789', owner);
+
+INSERT INTO `Users` (`username`, `email`, `password_hash`, `role`)
+VALUES ('deanweb', 'dean@example.com', 'password123', walker);
+
+INSERT INTO `Users` (`username`, `email`, `password_hash`, `role`)
+VALUES ('alice123', 'alice@example.com', 'hashed123', owner);
