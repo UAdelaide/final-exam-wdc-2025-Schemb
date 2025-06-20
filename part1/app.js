3159907896e927c929afea9e964d4d2451e54ab3
Schemb <joseph.schembri@student.adelaide.edu.au>
2025-06-20 12:14:29 +0000

    [automatic commit] 2025-06-20 12:14:29 UTC 115.131.34.193

--- part1/app.js
+++ part1/app.js
@@ -7,6 +7,16 @@ var mysql = require('mysql2/promise');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
+var apiRouter = require('./routes/api')(db);
+
+var app = express();
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
 let db;
 
 (async () => {
@@ -85,16 +95,6 @@ let db;
     console.error('Error connecting to database. Ensure dogwalks.sql is loaded into MySQL.', err);
   }
 })();
-
-var apiRouter = require('./routes/api')(db);
-
-var app = express();
-
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
