629658412bace1e6fd83a99e92ec1ac9d455bf7f
Schemb <joseph.schembri@student.adelaide.edu.au>
2025-06-20 11:42:16 +0000

    [automatic commit] 2025-06-20 11:42:16 UTC 115.131.34.193

--- part1/app.js
+++ part1/app.js
@@ -41,7 +41,7 @@ let db;
     }
 
     // Insert data if Dogs is empty
-    [rows] = await db.execute('SELECT COUNT(*) AS count FROM Users');
+    [rows] = await db.execute('SELECT COUNT(*) AS count FROM Dogs');
     if (rows[0].count === 0) {
       await db.execute(`
         INSERT INTO Users (username, email, password_hash, role) VALUES
