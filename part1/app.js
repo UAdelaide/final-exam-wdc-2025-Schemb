de3ee25024cd70db2010a400d0075ef312e2f901
Schemb <joseph.schembri@student.adelaide.edu.au>
2025-06-20 11:54:40 +0000

    [automatic commit] 2025-06-20 11:54:40 UTC 115.131.34.193

--- part1/app.js
+++ part1/app.js
@@ -70,7 +70,7 @@ let db;
     if (rows[0].count === 0) {
       await db.execute(`
         INSERT INTO WalkRequests (dog_id, requested_time, duration_minutes, location, status)
-        SELECT dog_id, '2025-06-10 08:00:00', 30, 'Parklands', 'open' FROM `Dogs` WHERE `name` = 'Max' LIMIT 1
+        SELECT dog_id, '2025-06-10 08:00:00', 30, 'Parklands', 'open' FROM Dogs WHERE name = 'Max' LIMIT 1
       `);
       await db.execute(`
         INSERT INTO Dogs (owner_id, name, size)
