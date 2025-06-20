571368a6a90074e0b1d8a67ab422f9bb3be7e1c9
Schemb <joseph.schembri@student.adelaide.edu.au>
2025-06-20 11:30:59 +0000

    [automatic commit] 2025-06-20 11:30:59 UTC 115.131.34.193

--- part1/app.js
+++ part1/app.js
@@ -35,7 +35,7 @@ let db;
       host: 'localhost',
       user: 'root',
       password: '',
-      database: 'testdb'
+      database: 'DogWalkService'
     });
 
     // Create a table if it doesn't exist
