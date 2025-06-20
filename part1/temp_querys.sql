6d758d32862ffaca412d087e7b9d7749b2ec64ad
Schemb <joseph.schembri@student.adelaide.edu.au>
2025-06-20 07:55:24 +0000

    [automatic commit] 2025-06-20 07:55:23 UTC 115.131.34.193

--- part1/temp_querys.sql
+++ part1/temp_querys.sql
@@ -37,4 +37,4 @@ VALUES ('josephs', 'joseph@example.com', 'password456', 'owner');
 
 -- Dogs Insert Into
 INSERT INTO `Dogs` (`owner_id`, `name`, `size`)
-SELECT `user_id`, 'Max', 'medium' FROM `Users` WHERE `username` = 'alice123' LIMIT 1;
+SELECT `user_id`, 'Max', 'medium' FROM `Users` WHERE `username` = 'alice123' AND `role` = 'owner' LIMIT 1;
