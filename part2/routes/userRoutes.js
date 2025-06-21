f0877358d6a6dbcfa35578466a786b5dbec92879
Schemb <joseph.schembri@student.adelaide.edu.au>
2025-06-21 06:20:21 +0000

    [automatic commit] 2025-06-21 06:20:21 UTC 115.131.34.193

--- part2/routes/userRoutes.js
+++ part2/routes/userRoutes.js
@@ -59,6 +59,8 @@ router.post('/login', async (req, res) => {
       return res.status(401).json({ error: 'Invalid credentials' });
     }
 
+    req.session.username = username;
+
     res.json({ message: 'Login successful', user: rows[0] });
   } catch (error) {
     res.status(500).json({ error: 'Login failed' });
