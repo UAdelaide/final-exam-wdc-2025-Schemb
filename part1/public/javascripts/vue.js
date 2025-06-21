37c8f505465c96e4b14d4ac031bab4f8386da8a1
Schemb <joseph.schembri@student.adelaide.edu.au>
2025-06-21 00:32:33 +0000

    [automatic commit] 2025-06-21 00:32:33 UTC 115.131.34.193

--- part1/public/javascripts/vue.js
+++ part1/public/javascripts/vue.js
@@ -7,7 +7,33 @@ const vueinst = Vue.createApp({
     },
     methods: {
         async fetchDog() {
-            
+            await fetch('async handleSignUp() {
+            try {
+                const response = await fetch('/signup', {
+                    method: 'POST',
+                    credentials: 'include',
+                    headers: {
+                        'Content-Type': 'application/json'
+                    },
+                    body: JSON.stringify({
+                        username: this.username,
+                        email: this.email,
+                        password: this.password
+                    })
+                });
+
+                if (!response.ok) {
+                    const errorData = await response.json();
+                    this.errorMessage = errorData.message || 'An error occurred.';
+                    return;
+                }
+
+                this.errorMessage = '';
+                window.location.href = '/';
+            } catch (error) {
+                this.errorMessage = 'Server error. Please try again later.';
+            }
+        },')
         }
     }
 }).mount('body');
