0e0d9e3c1314ae24c74a2b49ac12428120b82780
Schemb <joseph.schembri@student.adelaide.edu.au>
2025-06-21 00:36:08 +0000

    [automatic commit] 2025-06-21 00:36:08 UTC 115.131.34.193

--- part1/public/javascripts/vue.js
+++ part1/public/javascripts/vue.js
@@ -8,7 +8,7 @@ const vueinst = Vue.createApp({
     methods: {
         async fetchDog() {
             await fetch('https://dog.ceo/api/breeds/image/random', {
-                method: 'GET'
+                POST: 'GET'
                 headers: JSON.stringify({
                         username: this.username,
                         email: this.email,
