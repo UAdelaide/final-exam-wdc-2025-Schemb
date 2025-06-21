c2985f9bf5796d7813f5c04e6a3f1325f4c23abb
Schemb <joseph.schembri@student.adelaide.edu.au>
2025-06-21 00:56:19 +0000

    [automatic commit] 2025-06-21 00:56:19 UTC 115.131.34.193

--- part1/public/javascripts/vue.js
+++ part1/public/javascripts/vue.js
@@ -6,29 +6,25 @@ const vueinst = Vue.createApp({
             errorMessage: ''
         };
     },
-    // Vue functions
-    methods: {
-    },
     async mounted() {
-        // Function to fetch the Dog CEO API
-            try {
-                const response = await fetch('https://dog.ceo/api/breeds/image/random', {
-                    method: 'GET',
-                    headers: JSON.stringify({
-                            message: this.dogImage
-                    })
-                });
+        // Fetch the Dog CEO API
+        try {
+            const response = await fetch('https://dog.ceo/api/breeds/image/random', {
+                method: 'GET',
+                headers: JSON.stringify({
+                        message: this.dogImage
+                })
+            });
 
-                if (!response.ok) {
-                    const errorData = await response.json();
-                    this.errorMessage = errorData.message || 'An error occurred.';
-                    return;
-                }
-
-                this.errorMessage = '';
-            } catch (error) {
-                this.errorMessage = 'Could not fetch dog of the day. Please bring more treats.';
+            if (!response.ok) {
+                const errorData = await response.json();
+                this.errorMessage = errorData.message || 'An error occurred.';
+                return;
             }
+
+            this.errorMessage = '';
+        } catch (error) {
+            this.errorMessage = 'Could not fetch dog of the day. Please bring more treats.';
         }
     }
 }).mount('body');
