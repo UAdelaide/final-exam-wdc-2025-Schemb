1c71e3810f0f40ebb4ffb334d5b8747d22b0350e
Schemb <joseph.schembri@student.adelaide.edu.au>
2025-06-21 00:31:11 +0000

    [automatic commit] 2025-06-21 00:31:11 UTC 115.131.34.193

--- part1/public/javascripts/vue.js
+++ part1/public/javascripts/vue.js
@@ -2,8 +2,9 @@
 const vueinst = Vue.createApp({
     data() {
         return {
-            dogImageLink: 'https://dog.ceo/api/breeds/image/random'
+
         };
-    }
+    },
+    
 }).mount('body');
 window.vm = vueinst;
