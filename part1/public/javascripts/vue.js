03db468ac0afc58e671a71fc831855ffee8bef43
Schemb <joseph.schembri@student.adelaide.edu.au>
2025-06-21 00:55:38 +0000

    [automatic commit] 2025-06-21 00:55:38 UTC 115.131.34.193

--- part1/public/javascripts/vue.js
+++ part1/public/javascripts/vue.js
@@ -8,28 +8,9 @@ const vueinst = Vue.createApp({
     },
     // Vue functions
     methods: {
-        // Function to fetch the Dog CEO API
-        async fetchDog() {
-            try {
-                const response = await fetch('https://dog.ceo/api/breeds/image/random', {
-                    method: 'GET',
-                    headers: JSON.stringify({
-                            message: this.dogImage
-                    })
-                });
-
-                if (!response.ok) {
-                    const errorData = await response.json();
-                    this.errorMessage = errorData.message || 'An error occurred.';
-                    return;
-                }
-
-                this.errorMessage = '';
-            } catch (error) {
-                this.errorMessage = 'Could not fetch dog of the day. Please bring more treats.';
-            }
-        }
     },
-    async mounted()
+    async mounted() {
+
+    }
 }).mount('body');
 window.vm = vueinst;
