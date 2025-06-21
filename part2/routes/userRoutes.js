8ae0900ede8fba33dde56bf6e943f8f0cdd9f2ed
Schemb <joseph.schembri@student.adelaide.edu.au>
2025-06-21 06:44:51 +0000

    [automatic commit] 2025-06-21 06:44:51 UTC ;; communications error to 208.67.222.222#53: timed out 115.131.34.193

--- part2/routes/userRoutes.js
+++ part2/routes/userRoutes.js
@@ -82,7 +82,7 @@ router.post('/logout', async (req, res) => {
 
 router.get('/fetchDogs', async (req, res) => {
   const [rows] = await db.query(`
-      SELECT Dogs.name FROM Dogs INNER JOIN Users AT Dogs.owner_id = Users.user_id WHERE 
+      SELECT Dogs.name FROM Dogs INNER JOIN Users AT Dogs.owner_id = Users.user_id WHERE Users.user_id = ?
     `, [username, password]);
 
     if (rows.length === 0) {
